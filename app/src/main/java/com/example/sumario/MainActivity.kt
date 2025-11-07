8f7a03417b6ab5921b9141dea3c234a42b7806f8
ALYGITYORYO <jpalmahrz@gmail.com>
2025-11-06 20:47:25 -0600

    commit de andres

--- app/src/main/java/com/example/sumario/MainActivity.kt
+++ app/src/main/java/com/example/sumario/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SumarioTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "ANDRES",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
