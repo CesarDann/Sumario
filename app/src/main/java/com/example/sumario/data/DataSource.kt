38c4c66bb20aa02c15dd8e27bc296c3f151a61df
ALYGITYORYO <jpalmahrz@gmail.com>
2025-11-10 11:39:12 -0600

    crear data source y string

---
+++ app/src/main/java/com/example/sumario/data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.sumario.data
+
+object DataSource {
+    val info_integrantes = listOf(
+        Interfaz_data(R.string.architecture, 58, R.drawable.architecture),
+        Interfaz_data(R.string.crafts, 121, R.drawable.crafts),
+        Interfaz_data(R.string.business, 78, R.drawable.business),
+
+    )
+}
